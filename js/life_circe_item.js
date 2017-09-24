8a5945259e3fe82e483a4ff80404df0e5affefa5
公子倚 <1019596327@qq.com>
2017-09-24 20:57:44 +0800

    ..

---
+++ js/life_circe_item.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+	el:'#item',
+	data:{
+		message: 'Hello Vue!',
+		gao:0,
+		list:[
+		{
+			name:'我来组成标题',
+			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
+			content:'我来组成内容XXXXXXXX',
+			date:'2017-01-19',
+			time:'10：36',
+		},	
+		{
+			name:'我来组成标题',
+			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
+			content:'我来组成内容XXXXXXXX',
+			date:'2017-01-19',
+			time:'10：36',
+		},
+		{
+			name:'我来组成标题',
+			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
+			content:'我来组成内容XXXXXXXX',
+			date:'2017-01-19',
+			time:'10：36',
+		},
+		{
+			name:'我来组成标题',
+			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
+			content:'我来组成内容XXXXXXXX',
+			date:'2017-01-19',
+			time:'10：36',
+		},
+		{
+			name:'我来组成标题',
+			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
+			content:'我来组成内容XXXXXXXX',
+			date:'2017-01-19',
+			time:'10：36',
+		}]
+
+	},
+	methods:{
+	lifeCircle:function(){
+			
+
+		}
+	}
+})
