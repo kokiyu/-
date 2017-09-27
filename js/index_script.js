49162a34171417cc7d3754bf170a2621e7798431
kokiyu <651258027@qq.com>
2017-09-27 16:12:41 +0800

    BBS界面完成

--- js/index_script.js
+++ js/index_script.js
@@ -1,81 +1,89 @@
 var app = new Vue({
-el:'#all',
-data:{
-	HTitle:'生活圈',
-	beSelected:'life',
-	LifeImg:'images/Home_selected.png',
-	learnImg:"images/book.png",
-	BBSImg:"images/note.png",
-	MeImg:"images/my.png",
-	bodyCenter:"",
-	seen: true,
-},
-methods:{
-	returnGrey:function(){
-		console.log(this.beSelected);
-		if (this.beSelected == 'life') {
-			this.LifeImg ='images/home.png';
-
-		}
-		else if(this.beSelected == 'learn'){
-			this.learnImg='images/book.png';
-
-		}
-		else if (this.beSelected == 'BBS') {
-			this.BBSImg ='images/note.png';
-
-		}
-		else if (this.beSelected == 'my1'){
-			this.MeImg = 'images/my.png';
-		}
-		else{
-			return;
-		}
+	el:'#all',
+	data:{
+		HTitle:'生活圈',
+		beSelected:'life',
+		LifeImg:'images/Home_selected.png',
+		learnImg:"images/book.png",
+		BBSImg:"images/note.png",
+		MeImg:"images/my.png",
+		bodyCenter:"",
+		seen: true,
 	},
+	methods:{
+		returnGrey:function(){
+			console.log(this.beSelected);
+			if (this.beSelected == 'life') {
+				this.LifeImg ='images/home.png';
 
-	lifeCircle:function(){
-		this.HTitle = '生活圈';
-		this.LifeImg ='images/Home_selected.png';
-		this.seen = true;
-		this.returnGrey();
-		this.beSelected ='life';
+			}
+			else if(this.beSelected == 'learn'){
+				this.learnImg='images/book.png';
+
+			}
+			else if (this.beSelected == 'BBS') {
+				this.BBSImg ='images/note.png';
+
+			}
+			else if (this.beSelected == 'my1'){
+				this.MeImg = 'images/my.png';
+			}
+			else{
+				return;
+			}
+		},
+
+		lifeCircle:function(){
+			this.HTitle = '生活圈';
+			this.LifeImg ='images/Home_selected.png';
+			this.seen = true;
+			this.returnGrey();
+			this.beSelected ='life';
 
 
-	},
-	learnCircle:function(event){
-		this.HTitle = '学习圈';
-		this.learnImg = 'images/book_selected.png';
-		this.seen = false;
-		this.bodyCenter = 'Learn.html';
-		this.returnGrey();
-		this.beSelected = 'learn';
-	},
-	BBSCircle:function(event){
-		this.HTitle = '党建论坛';
-		this.BBSImg = 'images/Note_selected.png';
-		this.seen = true;
-		this.bodyCenter = 'BBS.html';
-		this.returnGrey();
-		this.beSelected = 'BBS';
+		},
+		learnCircle:function(event){
+			this.HTitle = '学习圈';
+			this.learnImg = 'images/book_selected.png';
+			this.seen = false;
+			this.bodyCenter = 'Learn.html';
+			this.returnGrey();
+			this.beSelected = 'learn';
+		},
+		BBSCircle:function(event){
+			this.HTitle = '党建论坛';
+			this.BBSImg = 'images/Note_selected.png';
+			this.seen = true;
+			this.bodyCenter = 'BBS.html';
+			this.returnGrey();
+			this.beSelected = 'BBS';
 
 
-	},
-	MyCircle:function(event){
-		this.HTitle = '我的档案';
-		this.MeImg = 'images/my_selected.png';
-		this.seen = false;
-		this.returnGrey();
-		this.beSelected = 'my1';
-		this.bodyCenter = 'setting.html';
+		},
+		MyCircle:function(event){
+			this.HTitle = '我的档案';
+			this.MeImg = 'images/my_selected.png';
+			this.seen = false;
+			this.returnGrey();
+			this.beSelected = 'my1';
+			this.bodyCenter = 'setting.html';
 
-	}
+		},
+		openSon:function(event){
+			if (this.HTitle == '党建论坛') {
+				window.location.href=("./addMessage.html");	
+			}else{
+				window.location.href=("./life.html");	
+			}
 
-},
-mounted: function () {
-	this.$nextTick(function () {
+			},
+
+		},
+		mounted: function () {
+			this.$nextTick(function () {
 // Code that will run only after the
 // entire view has been rendered
 })
-}
+		}
 
-})
+	})
