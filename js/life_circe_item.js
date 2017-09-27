bc01c73b32f5baa806653e94b6c6e757a71ddce7
公子倚 <1019596327@qq.com>
2017-09-27 20:56:50 +0800

    Revert "完成首页"
    This reverts commit 37438cfd1e55cd3c1fd615e42802c4d868ff8721.

--- js/life_circe_item.js
+++ js/life_circe_item.js
@@ -2,8 +2,7 @@ var app = new Vue({
 	el:'#item',
 	data:{
 		message: 'Hello Vue!',
-		loding_text:'加载更多',
-		loding_index:0,
+		gao:0,
 		list:[
 		{
 			name:'我来组成标题',
@@ -22,13 +21,6 @@ var app = new Vue({
 		{
 			name:'我来组成标题',
 			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
-			content:'《荷塘月色》是中国文学家朱自清任教清华大学时所写的一篇散文，因收入中学语文教材而广为人知，是现代抒情散文的名篇。文章写了荷塘月色美丽的景象，含蓄而又委婉地抒发了作者不满现实，渴望自由，想超脱现实而又不能的复杂的思想感情，为后人留下了旧中国正直知识分子在苦难中徘徊前进的足迹。寄托了作者一种向往于未来的政治思想，也寄托了作者对荷塘月色的喜爱之情。',
-			date:'2017-01-19',
-			time:'10：36',
-		},
-		{
-			name:'我来组成标题',
-			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
 			content:'我来组成内容XXXXXXXX',
 			date:'2017-01-19',
 			time:'10：36',
@@ -46,41 +38,12 @@ var app = new Vue({
 			content:'我来组成内容XXXXXXXX',
 			date:'2017-01-19',
 			time:'10：36',
-		},	
-		{
-			name:'我来组成标题',
-			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
-			content:'我来组成内容XXXXXXXX',
-			date:'2017-01-19',
-			time:'10：36',
-		},
-		{
-			name:'我来组成标题',
-			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
-			content:'《荷塘月色》是中国文学家朱自清任教清华大学时所写的一篇散文，因收入中学语文教材而广为人知，是现代抒情散文的名篇。文章写了荷塘月色美丽的景象，含蓄而又委婉地抒发了作者不满现实，渴望自由，想超脱现实而又不能的复杂的思想感情，为后人留下了旧中国正直知识分子在苦难中徘徊前进的足迹。寄托了作者一种向往于未来的政治思想，也寄托了作者对荷塘月色的喜爱之情。',
-			date:'2017-01-19',
-			time:'10：36',
-		},
-		{
-			name:'我来组成标题',
-			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
-			content:'我来组成内容XXXXXXXX',
-			date:'2017-01-19',
-			time:'10：36',
-		},
-		{
-			name:'我来组成标题',
-			icon:'http://img0.imgtn.bdimg.com/it/u=808042276,2411136752&fm=27&gp=0.jpg',
-			content:'我来组成内容XXXXXXXX',
-			date:'2017-01-19',
-			time:'10：36',
-		}
-		]
+		}]
 
 	},
 	methods:{
-	reload:function(){
-			console.log("sssssssssssssssssss")
+	lifeCircle:function(){
+			
 
 		}
 	}
